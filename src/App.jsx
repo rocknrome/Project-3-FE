902724986e3b195e557658d288cf9435c66cde44
Roman Larionov <pcsaveddotcom@gmail.com>
2024-02-22 14:38:17 -0500

    user auth working

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react" /*importing SignIn and SignOut btns*/
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <SignedOut>
+          <SignInButton />
+              <h3>This content is PUBLIC. Only signed out users can see this.</h3>
+        </SignedOut>
+
+        <SignedIn>
+          <SignOutButton signOutCallback={() => redirect('/')} />
+              <h3>This content is PRIVATE. Only signed in users can see this.</h3>
+        </SignedIn>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+
+// Importing our publishable key for Clerk
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+//Running check for the key
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}> {/*wrapping app in ClerkProvider*/}
+        <App />
+      </ClerkProvider>
   </React.StrictMode>,
 )
